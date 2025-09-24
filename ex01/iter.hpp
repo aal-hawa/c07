6e6ef25addf671efc1d6a755cde7af043f988ac8
Anas Al Hawamda <aal-hawa@lab3r7s3.42abudhabi.ae>
2025-09-24 16:53:05 +0400

    edit

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,14 +3,16 @@
 
 #include <cstddef>
 
-template<typename T>
-void iter(T const *array, size_t length, void (*func)(T const &))
-{
-	for (size_t i = 0; i < length; ++i)
-		func(array[i]);
-}
-template<typename T>
-void iter(T *array, size_t length, void (*func)(T &))
+// template<typename T, typename F>
+// // template<typename T>
+// void iter(T const *array, size_t length, F func)
+// {
+// 	for (size_t i = 0; i < length; ++i)
+// 		func(array[i]);
+// }
+// template<typename T>
+template<typename T, typename F>
+void iter(T *array, const size_t length, F func)
 {
 	for (size_t i = 0; i < length; ++i)
 		func(array[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -21,32 +21,36 @@ int main( void )
 		size_t len = sizeof(arr) / sizeof(int);
 		
 		std::cout << "Original int array: ";
-		iter(arr, len, printElement);
+		iter(arr, len, printElement<const int>);
 		std::cout << "\n";
 		
-		iter(arr, len, incrementElement);
+		iter(arr, len, incrementElement<int>);
 		
 		std::cout << "After increment: ";
-		iter(arr, len, printElement);
+		iter(arr, len, printElement<const int>);
 		std::cout << "\n";
 	}
 
 	{
 		std::string arr[] = {"Hello", "World", "!"};
 		size_t len = sizeof(arr) / sizeof(std::string);
+		// size_t len = arr->size();
+		// arr->size();
 		std::cout << "String array: ";
-		iter(arr, len, printElement);
+		iter(arr, len, printElement<const std::string>);
 		std::cout << "\n";
 	}
 
 	{
 		const char arr[] = {'A', 'B', 'C'};
 		size_t len = sizeof(arr) / sizeof(char);
-
+		
 		std::cout << "Const char array: ";
-		iter(arr, len, printElement);
+		iter(arr, len, printElement<const char>);
 		
 		std::cout << "\n";
 	}
+	std::cout << "\n";
+
 	return 0;
 }
