59066453733b2f3ee2acdd4fd0cf28c4a78f0a28
Anas Al Hawamda <aal-hawa@lab1r3s7.42abudhabi.ae>
2025-09-28 14:42:51 +0400

    edit

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -3,14 +3,7 @@
 
 #include <cstddef>
 
-// template<typename T, typename F>
-// // template<typename T>
-// void iter(T const *array, size_t length, F func)
-// {
-// 	for (size_t i = 0; i < length; ++i)
-// 		func(array[i]);
-// }
-// template<typename T>
+
 template<typename T, typename F>
 void iter(T *array, const size_t length, F func)
 {
